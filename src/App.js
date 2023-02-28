539692dfd8cbc8d9d3b2b92760bee168fbd5d89c
NastiaShulgina <75643824+NastiaShulgina@users.noreply.github.com>
2023-02-28 17:50:09 +0200

    1.7: unicafe step2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import { useState } from 'react'
 
-const Button = ({ name, state, setState }) => {
+const Button = ({ name, state, good, bad, setState, setAverage, all, setAll, setPositive }) => {
+  const handleClicks = () => {
+    const updatedState = state + 1
+    setState(updatedState)
+    const updatedAll = all + 1
+    setAll(updatedAll)
+    if (name === "good") {
+      setAverage((updatedState - bad) / updatedAll)
+      setPositive(updatedState * 100 / updatedAll)
+    }
+    else if (name === "bad") {
+      setAverage((good - updatedState) / updatedAll)
+      setPositive(good * 100 / updatedAll)
+    }
+  }
+
   return (
     <span>
-      <button onClick={() => setState(state + 1)}>{name}</button><span>&nbsp;</span>
+      <button onClick={handleClicks}>{name}</button><span>&nbsp;</span>
     </span>
   );
 }
@@ -13,6 +28,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [all, setAll] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const buttons = [
     {
@@ -37,19 +55,23 @@ const App = () => {
       {
         buttons.map(button => {
           return <span>
-            <Button name={button.name} state={button.state} setState={button.setState} onClick={() => button.setState(button.state + 1)} />
+            <Button name={button.name} state={button.state} good={good} bad={bad} setState={button.setState}
+              all={all} setAll={setAll} setAverage={setAverage} setPositive={setPositive} />
           </span>
         })
       }
-      <h1>statistics</h1>
       {
         buttons.map(button => {
           return <div>
-            <span>{button.name} &nbsp;</span>
-            <span>{button.state}</span>
+            <span>{button.name} {button.state}</span>
           </div>
         })
       }
+      <div>
+        <div>all {all}</div>
+        <div>average {average}</div>
+        <div>positive {positive} %</div>
+      </div>
     </div>
   )
 }
